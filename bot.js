2db4978a0254764312172fec7ee5b35b88178e39
Ryan <ryansousis@gmail.com>
2020-05-06 23:30:44 -0400

    Testing basic commands

--- bot.js
+++ bot.js
@@ -11,6 +11,39 @@ client.on('message', msg => {
     if(msg.content === 'ping'){
         msg.reply('pong');
     }
+
+    if(msg.content.substring(0,1) === '!') {
+        var args = msg.content.substring(1).split(' ');
+        console.log(args);
+        var cmd = args[0];
+
+        args = args.splice(1);
+        console.log(args);
+        switch(cmd)
+        {
+            case 'ping':
+                msg.reply('!pong');
+                break;
+            case 'yeet':
+                yeet(args[0], args[1], msg);
+        }
+    }
+});
+
+client.on('guildMemberAdd', member => {
+    const channel = member.guild.channels.cache.find(ch => ch.name === 'alerts');
+
+    if(!channel) return;
+
+    channel.send(`Welcome to the server, ${member}, you glorious hawk`);
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+function yeet(user, distance, msg){
+    if(typeof(user) === "string" /*&& typeof(distance) === "number"*/)
+    {
+        msg.reply(typeof(distance));
+        msg.reply(`You throw ${user} ${distance} feet`);
+    }
+}
